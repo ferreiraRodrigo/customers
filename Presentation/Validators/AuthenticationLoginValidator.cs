153abf79f35a66fd5712b4c79f232ce1bb7b5357
rodrigoferreira <ferreira.rodrigosf@gmail.com>
2022-04-21 22:25:49 -0300

    adding dto validators

---
+++ Presentation/Validators/AuthenticationLoginValidator.cs
@@ -0,0 +1,27 @@
+﻿using Customers.Presentation.Dtos.Authentication;
+using FluentValidation;
+
+namespace Customers.Presentation.Validators
+{
+    public class AuthenticationLoginValidator : AbstractValidator<AuthenticationLoginDTO>
+    {
+        public AuthenticationLoginValidator()
+        {
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .WithMessage("Email is required")
+                .EmailAddress()
+                .WithMessage("Email is not valid");
+
+            RuleFor(x => x.Password)
+                .NotEmpty()
+                .WithMessage("Password is required")
+                .MinimumLength(8)
+                .WithMessage("Password must be at least 8 characters long.");
+            
+            RuleFor(x => x.Scopes)
+                .NotEmpty()
+                .WithMessage("Scopes are required.");
+        }
+    }
+}
---
+++ Presentation/Validators/CustomerUpdateValidator.cs
@@ -0,0 +1,15 @@
+﻿using Customers.Presentation.Dtos;
+using FluentValidation;
+
+namespace Customers.Presentation.Validators
+{
+    public class CustomerUpdateValidator : AbstractValidator<CustomerUpdateDTO>
+    {
+        public CustomerUpdateValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .WithMessage("Name is required");
+        }
+    }
+}
